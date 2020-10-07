382fe5004a271d61ac0507d04936668319a9fd4a
Ted <60035697+TLBones@users.noreply.github.com>
2020-10-07 17:58:57 -0600

    Added Files

---
+++ blog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1200)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1200)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    base_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+#Created Model and createView. Need to make it so it actually visualizes. Find way to implement createview within template.
+
+
+
+
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tedbonham/Desktop/teds-tree/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
